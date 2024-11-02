60648b8bc415b16a75161316e504db6f816015c3
machadoluan <machado.luandealmeida@gmail.com>
2024-11-02 18:09:35 -0300

    Adicionando a biblioteca toast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,21 @@ import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'login-discord';
 
-  constructor(private router: Router) {}
-  
+  constructor(private router: Router) { }
+
   isRegistrationRoute(): boolean {
-    return this.router.url === '/registro'; // ajuste o path conforme sua rota
+    return this.router.url === '/cadastro'; // ajuste o path conforme sua rota
   }
 }
